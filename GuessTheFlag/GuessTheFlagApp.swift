b330e491803f5e4b0bb6ef760bd41fdf1490f964
Ilyas Aslan <152633183+quatarnary@users.noreply.github.com>
2024-02-12 15:32:13 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Bugra Aslan on 12.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
